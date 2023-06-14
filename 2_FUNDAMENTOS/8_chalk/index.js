c1b209e1b3a5f9d969d5d8be213ef26156cbab9e
Juanhen10 <henrqiuejuan36@gmail.com>
2023-06-13 21:12:28 -0300

    NodeJS

---
+++ 2_FUNDAMENTOS/8_chalk/index.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk')
+
+const nota = 3
+
+
+if(nota >= 7){
+    console.log(chalk.green('Parabens! Vocẽ está aprovado'))
+} else{
+    console.log(chalk.bgRed.black.bold('Você foi reprovado'))
+}
